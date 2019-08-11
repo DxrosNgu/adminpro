97f65f4c5da9c6d88fffd426d10e581b1cb1a36b
danyosa <danyosa@csticorp.biz>
2019-08-11 06:23:51 -0500

    Sección 8 terminada

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,15 +4,19 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {ProgressComponent} from './progress/progress.component';
 import {Graphics1Component} from './graphics1/graphics1.component';
 import {AccountSettingsComponent} from './account-settings/account-settings.component';
+import {PromesasComponent} from './promesas/promesas.component';
+import {RxjsComponent} from './rxjs/rxjs.component';
 
 const pagesRoutes: Routes = [
   {path: '',
   component: PagesComponent,
     children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'graficas1', component: Graphics1Component},
-      {path: 'account-settings', component: AccountSettingsComponent},
+      {path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+      {path: 'progress', component: ProgressComponent, data: {titulo: 'Progress'}},
+      {path: 'graficas1', component: Graphics1Component, data: {titulo: 'Graficos'} },
+      {path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
+      {path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajuste del Tema'} },
+      {path: 'rxjs', component: RxjsComponent, data: {titulo: 'Rxjs'} },
       {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
     ]
   }
